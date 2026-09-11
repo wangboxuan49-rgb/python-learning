1a6ccb2d81b011d2ef756d972061cd17cd16e414
wangboxuan49-rgb <wangboxuan49@gmail.com>
2026-09-11 16:05:27 +0800

    ch02:完成第二章习题

---
+++ ch02/number8.py
@@ -0,0 +1,12 @@
+#  功能:计算数字 8
+#  作者:wbx
+#  日期:2026-09-11
+
+#利用加减乘除来计算
+print(3+5)
+print(10.0-2)   #操作数有一个是浮点数，结果也必然是浮点数
+print(2*4)
+print(80/10)    #除法计算出必是浮点数
+
+fav_num = 8
+print(f"My favorite number is {fav_num}")       #f字符串可以直接用于 print 当中，更加简洁
