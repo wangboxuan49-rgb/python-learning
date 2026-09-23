53de4b379b9fe2111b344fb0f6b8daf7c977798a
wangboxuan49-rgb <wangboxuan49@gmail.com>
2026-09-23 11:53:54 +0800

    ch07:修改第七章练习题

--- ch07/cinema_ticket.py
+++ ch07/cinema_ticket.py
@@ -3,12 +3,12 @@
 #  日期:2026-09-23
 
 prompt = '请输入您的年龄：（输入 quit 退出）'
-active = True
+active = True       #使用标志来作为循环退出判断
 
 while active:
     age = input(prompt)
 
-    if age == 'quit':
+    if age == 'quit':       #要使用if 语句，否则输入 quit 之后会将本轮循环后续运行完
         active = False
     else:
         age = int(age)
--- ch07/integer_multiple.py
+++ ch07/integer_multiple.py
@@ -3,13 +3,13 @@
 #  日期:2026-09-23
 
 while True:     #无限循环，遇到break退出循环
-    num = input("请输入一个数：（输入 q 退出）")
-    if num =='q':
+    num = input("请输入一个数：（输入 q 退出）")    #获取用户输入，并显示一个提示
+    if num =='q':   #使用if语句能够判断退出循环条件
         break
 
-    num = int(num)
+    num = int(num)      #input（）函数输入为字符串，用于数值比较时可以用int（）函数转换为数值
 
-    if num % 10 == 0:
+    if num % 10 == 0:       #取模运算
         print(f"{num}是十的整数倍")
     else:
         print(f"{num}不是十的整数倍")
